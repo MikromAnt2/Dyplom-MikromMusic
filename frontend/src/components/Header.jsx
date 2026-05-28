2f83baa8adeb58216878f20b24b56e0a63f698bc
MikromAnt <artuhow5@gmail.com>
2026-05-28 15:20:19 +0300

    fixed visual bugs

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -64,14 +64,13 @@ export default function Header() {
                     <NavLink to="/" className="logo" title={t('header.logoTitle')}>
                         <img src={siteLogo} alt="Mikrom" />
                     </NavLink>
-                </div>
-
-                <div className="header_right">
                     <nav className="header_nav">
                         <NavLink to="/" className="nav_item" end>{t('header.home')}</NavLink>
                         <NavLink to="/favorites" className="nav_item" id="nav_favorites">{t('header.library')}</NavLink>
                     </nav>
+                </div>
 
+                <div className="header_right">
                     <div className="search_bar">
                         <span className="search_icon_wrapper"><img src={searchIcon} alt="" /></span>
                         <input
